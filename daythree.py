e2ff54fb836224c6940461044fc362185179d046
stephenleeustc <stephen.lee.ustc@gmail.com>
2017-12-08 15:18:35 +0800

    daythree part2 com new point: yeild from and zip(*matrix), how it works

--- daythree.py
+++ daythree.py
@@ -13,3 +13,12 @@ def judge(triangle):
 assert judge([3, 1, 8]) == False
 
 print(sum(map(judge, [parse_line(line) for line in lt.Input(3)])))
+
+traingles = [parse_line(line) for line in lt.Input(3)]
+
+def invert(triangles):
+    "invert the matrix"
+    for i in range(0, len(traingles), 3):
+        yield from lt.transpose(traingles[i:i+3])
+
+print(sum(map(judge, invert(traingles))))
