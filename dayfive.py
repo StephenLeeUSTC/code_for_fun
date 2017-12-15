6f4cef314f3bee6c419bd60312ed8b6c3049dc67
stephenleeustc <stephen.lee.ustc@gmail.com>
2017-12-15 22:24:31 +0800

    day5 part2, almost same with part1, need to remember int('xxx', base = 16)

--- dayfive.py
+++ dayfive.py
@@ -14,6 +14,20 @@ def parse_letter(letters):
         if len(password) == 8:
             return password
 
-assert parse_letter('abc') == '18f47a30'
+# assert parse_letter('abc') == '18f47a30'
 
-print(parse_letter(secret))
+# print(parse_letter(secret))
+
+def get_password(letters):
+    password = ['-'] * 8
+    for i in range(10 ** 99):
+        mix = hashlib.md5((letters + str(i)).encode('utf-8')).hexdigest()
+        if mix.startswith('00000'):
+            index = int(mix[5], 16)
+            if index < 8 and password[index] is '-':
+                password[index] = mix[6]
+                print(password)
+            if '-' not in password:
+                return password
+
+print('password is ', ''.join(get_password(secret)))
