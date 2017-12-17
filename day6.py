d025e70be9a681d994a1a358aa9f3fbc2e66bbb4
stephenleeustc <stephen.lee.ustc@gmail.com>
2017-12-17 15:35:02 +0800

    complete day6 1. collections.Counter is useful, most_common() return a list of list as it's word meanning 2. split() is very interesting 3. fix code style in last commit, i think just use most_common() is enough

--- day6.py
+++ day6.py
@@ -6,10 +6,11 @@ counts = [Counter(col) for col in zip(*lt.Input(6).read().split())]
 # print(lt.Input(6).read())
 # print(lt.Input(6).read().split())
 
-answer1 = ''.join(e.most_common(1)[0][0] for e in counts)
+
+answer1 = ''.join(e.most_common()[0][0] for e in counts)
 print(answer1)
 
-answer2 = ''.join(e.most_common()[-1][0][0] for e in counts)
+answer2 = ''.join(e.most_common()[-1][0] for e in counts)
 print(answer2)
 
 
